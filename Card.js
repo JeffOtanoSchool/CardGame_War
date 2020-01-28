e12059f79c7e690977222108ab43562e5c3dc0fe
JeffOtano <otanojeffrey@gmail.com>
2020-01-28 00:26:22 -0800

    Finished game

--- Card.js
+++ Card.js
@@ -1,28 +1,28 @@
 const suit = [
-    'Hearts',
-    'Clubs',
-    'Spades',
-    'Diamonds',
-]
+  { Suit: "Clubs", Value: 1 },
+  { Suit: "Diamonds", Value: 2 },
+  { Suit: "Hearts", Value: 3 },
+  { Suit: "Spades", Value: 4 }
+];
 
 const rank = [
-    { Face:'Ace', Value:14},
-    { Face:'King', Value:13},
-     { Face:'Queen', Value:12},
-     { Face:'Jack', Value:11},
-     { Face:'Ten', Value:10},
-     { Face:'Nine', Value:9},
-     { Face:'Eight', Value:8},
-     { Face:'Seven', Value:7},
-     { Face:'Six', Value:6},
-     { Face:'Five', Value:5},
-     { Face:'Four', Value:4},
-     { Face:'Three', Value:3},
-     { Face:'Two', Value:2},
-]
+  { Face: "Ace", Value: 14 },
+  { Face: "King", Value: 13 },
+  { Face: "Queen", Value: 12 },
+  { Face: "Jack", Value: 11 },
+  { Face: "Ten", Value: 10 },
+  { Face: "Nine", Value: 9 },
+  { Face: "Eight", Value: 8 },
+  { Face: "Seven", Value: 7 },
+  { Face: "Six", Value: 6 },
+  { Face: "Five", Value: 5 },
+  { Face: "Four", Value: 4 },
+  { Face: "Three", Value: 3 },
+  { Face: "Two", Value: 2 }
+];
 
-const Card = function(suit,rank) {
+const Card = function(suit, rank) {
   this.suit = suit;
   this.rank = rank;
   this.inUse = false;
-}
+};
--- main.js
+++ main.js
@@ -1,45 +1,132 @@
 let playerArray = [];
 let computerArray = [];
+let cardPlace = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 var dealButton = document.querySelector("#deal"),
-nextButton = document.querySelector("#next");
+  nextButton = document.querySelector("#next");
 
-dealButton.addEventListener("click",deal,false);
-nextButton.addEventListener('click',next,false);
-nextButton.style.display = 'none';
+dealButton.addEventListener("click", deal, false);
+nextButton.addEventListener("click", next, false);
+nextButton.style.display = "none";
 
-
-function deal(){
+function deal() {
   Deck.load();
-  let cardCount = 52;
-  while(playerArray.length !== 26)
-  {
+  const cardCount = 52;
+  while (playerArray.length !== 26) {
     const random = Math.floor(Math.random() * cardCount);
     let nextPossibleCard = Deck.cardArray[random];
-    if(!nextPossibleCard.inUse)
-    {
+    if (!nextPossibleCard.inUse) {
       playerArray.push(nextPossibleCard);
       Deck.cardArray[random].inUse = true;
     }
   }
 
   for (const possibleCard of Deck.cardArray) {
-  if(!possibleCard.inUse)
-  {
-    computerArray.push(possibleCard);
+    if (!possibleCard.inUse) {
+      computerArray.push(possibleCard);
+    }
+  }
+
+  dealButton.style.display = "none";
+  nextButton.style.display = "block";
+}
+
+function next() {
+  if (cardPlace < 26) {
+    displayTurnNumber();
+    const currentPlayerCard = playerArray[cardPlace];
+    const currentComputerCard = computerArray[cardPlace];
+
+    displayCards(currentPlayerCard, currentComputerCard);
+    calculateWinner(currentPlayerCard, currentComputerCard);
+    cardPlace++;
+  } else {
+    resetGame();
   }
 }
 
-  console.log(playerArray.length);
-  console.log(computerArray.length);
-  console.log(Deck);
-
-  dealButton.style.display = 'none';
-  nextButton.style.display = 'block';
+function displayTurnNumber(reset) {
+  if (reset) {
+    document.getElementById("turnNumber").innerHTML = 0;
+    return;
+  }
+  document.getElementById("turnNumber").innerHTML = cardPlace + 1;
 }
 
-function next(){
-  
+function displayCards(playerCard, computerCard) {
+  if (playerCard === undefined && computerCard === undefined) {
+    document.getElementById("playerCard").innerHTML = "";
+    document.getElementById("computerCard").innerHTML = "";
+    return;
+  }
 
+  setCardColor(playerCard, "playerCard");
+  document.getElementById("playerCard").innerHTML =
+    playerCard.rank.Face + " of " + playerCard.suit.Suit;
+  setCardColor(computerCard, "computerCard");
+  document.getElementById("computerCard").innerHTML =
+    computerCard.rank.Face + " of " + computerCard.suit.Suit;
+}
 
+function setCardColor(card, id) {
+  if (card.suit.Value === 1 || card.suit.Value === 2) {
+    document.getElementById(id).style.color = "red";
+  } else {
+    document.getElementById(id).style.color = "black";
+  }
+}
+
+function calculateWinner(playerCard, computerCard) {
+  if (playerCard.rank.Value > computerCard.rank.Value) {
+    playerScore++;
+    displayScores(playerScore, computerScore, "won");
+  } else if (playerCard.rank.Value === computerCard.rank.Value) {
+    if (playerCard.suit.Value > computerCard.suit.Value) {
+      playerScore++;
+      displayScores(playerScore, computerScore, "won");
+    } else {
+      computerScore++;
+      displayScores(playerScore, computerScore, "lost");
+    }
+  } else {
+    computerScore++;
+    displayScores(playerScore, computerScore, "lost");
+  }
+}
+
+function displayScores(playerScore, computerScore, outcome) {
+  if (playerScore === undefined && computerScore === undefined) {
+    document.getElementById("playerScore").innerHTML = 0;
+    document.getElementById("computerScore").innerHTML = 0;
+    document.getElementById("outcome").innerHTML = "";
+    return;
+  }
+
+  if (cardPlace === 25) {
+    document.getElementById("outcome").innerHTML =
+      playerScore > computerScore
+        ? "YOU WON!"
+        : playerScore === computerScore
+        ? "It was a tie :("
+        : "YOU LOST!";
+  } else {
+    document.getElementById("outcome").innerHTML =
+      "You " + outcome + " this hand.";
+  }
+  document.getElementById("playerScore").innerHTML = playerScore;
+  document.getElementById("computerScore").innerHTML = computerScore;
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  cardPlace = 0;
+  displayCards();
+  displayScores();
+  displayTurnNumber(true);
+
+  dealButton.style.display = "block";
+  nextButton.style.display = "none";
 }
